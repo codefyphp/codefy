f4ccc0c4ea86cd4857a4f2276c5818dab483b1c5
Joshua Parker <josh@joshuaparker.blog>
2022-09-23 07:26:21 -0700

    Updated composer.

--- Migration/Adapter/MigrationAdapter.php
+++ Migration/Adapter/MigrationAdapter.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Codefy\Foundation\Migration\Adapter;
 
 use Codefy\Foundation\Migration\Migration;
-use Qubus\Dbal\Connection;
 
 interface MigrationDatabaseAdapter
 {
