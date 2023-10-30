31a2aa7cd9d62259588589bb9a7be720855bf58e
Joshua Parker <joshua@joshuaparker.dev>
2023-10-30 15:15:18 -0600

    Added FileSmtpLoggerFactory.

--- Codefy.php
+++ Codefy.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Codefy\Framework;
 
 use Codefy\Framework\Factory\FileLoggerFactory;
+use Codefy\Framework\Factory\FileLoggerSmtpFactory;
 use Psr\Log\LoggerInterface;
+use Qubus\Exception\Exception;
 use ReflectionException;
 
 final class Codefy
@@ -25,5 +27,16 @@ final class Codefy
     public static function getLogger(): LoggerInterface
     {
         return FileLoggerFactory::getLogger();
+    }
+
+    /**
+     * FileLogger with SMTP support.
+     *
+     * @throws ReflectionException
+     * @throws Exception
+     */
+    public static function getSmtpLogger(): LoggerInterface
+    {
+        return FileLoggerSmtpFactory::getLogger();
     }
 }
