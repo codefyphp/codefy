7232703715166b314daa15e22b01402fc5a74467
Joshua Parker <joshua@joshuaparker.dev>
2024-09-01 19:01:43 -0700

    Removed logger from Codefy.php.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- Codefy.php
+++ Codefy.php
@@ -4,12 +4,6 @@ declare(strict_types=1);
 
 namespace Codefy\Framework;
 
-use Codefy\Framework\Factory\FileLoggerFactory;
-use Codefy\Framework\Factory\FileLoggerSmtpFactory;
-use Psr\Log\LoggerInterface;
-use Qubus\Exception\Exception;
-use ReflectionException;
-
 final class Codefy
 {
     /**
@@ -18,24 +12,4 @@ final class Codefy
      * @var Application $php
      */
     public static Application $PHP;
-
-    /**
-     * FileLogger
-     *
-     * @throws ReflectionException
-     */
-    public static function getLogger(): LoggerInterface
-    {
-        return FileLoggerFactory::getLogger();
-    }
-
-    /**
-     * FileLogger with SMTP support.
-     *
-     * @throws ReflectionException
-     */
-    public static function getSmtpLogger(): LoggerInterface
-    {
-        return FileLoggerSmtpFactory::getLogger();
-    }
 }
