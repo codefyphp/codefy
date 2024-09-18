35e6615dea5df4feb0e9b3ea2bcb36545e3ac91c
Joshua Parker <joshua@joshuaparker.dev>
2024-09-18 16:13:10 -0700

    Removed the boot method.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- Providers/RbacServiceProvider.php
+++ Providers/RbacServiceProvider.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Codefy\Framework\Providers;
 
-use Codefy\Framework\Auth\Rbac\RbacLoader;
 use Codefy\Framework\Auth\Rbac\Resource\ConfigStorageResource;
 use Codefy\Framework\Auth\Rbac\Resource\StorageResource;
 use Codefy\Framework\Support\CodefyServiceProvider;
-use Qubus\Exception\Exception;
 
 final class RbacServiceProvider extends CodefyServiceProvider
 {
@@ -20,21 +18,5 @@ final class RbacServiceProvider extends CodefyServiceProvider
 
         $this->codefy->alias(original: StorageResource::class, alias: ConfigStorageResource::class);
         $this->codefy->share(nameOrInstance: StorageResource::class);
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function boot(): void
-    {
-        if ($this->codefy->isRunningInConsole()) {
-            return;
-        }
-
-        /** @var RbacLoader $rbac */
-        $rbac = $this->codefy->make(name: RbacLoader::class);
-
-        $rbac->initRbacRoles();
-        $rbac->initRbacPermissions();
     }
 }
