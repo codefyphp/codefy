a829f8793de8789336c09d33ded9954c44295a21
Joshua Parker <joshua@joshuaparker.dev>
2024-10-18 11:13:54 -0700

    Updated service provider to run in the console.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- Providers/PdoServiceProvider.php
+++ Providers/PdoServiceProvider.php
@@ -7,28 +7,25 @@ namespace Codefy\Framework\Providers;
 use Codefy\Framework\Support\CodefyServiceProvider;
 use PDO;
 
-use function Codefy\Framework\Helpers\env;
+use function Codefy\Framework\Helpers\config;
 use function sprintf;
 
 final class PdoServiceProvider extends CodefyServiceProvider
 {
     public function register(): void
     {
-        if ($this->codefy->isRunningInConsole()) {
-            return;
-        }
-
+        $default = config(key: 'database.default');
         $dsn = sprintf(
             '%s:dbname=%s;host=%s',
-            env(key: 'DB_DRIVER'),
-            env(key: 'DB_NAME'),
-            env(key:'DB_HOST')
+            config(key: "database.connections.{$default}.driver"),
+            config(key: "database.connections.{$default}.dbname"),
+            config(key: "database.connections.{$default}.host")
         );
 
         $this->codefy->define(name: PDO::class, args: [
             ':dsn' => $dsn,
-            ':username' => env(key: 'DB_USER'),
-            ':passwd' => env(key: 'DB_PASSWORD'),
+            ':username' => config(key: "database.connections.{$default}.username"),
+            ':passwd' => config(key: "database.connections.{$default}.password"),
         ]);
 
         $this->codefy->share(nameOrInstance: PDO::class);
