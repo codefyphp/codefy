c832457dfb78f8c1d85d37b99147d1dd61214354
Joshua Parker <joshua@joshuaparker.dev>
2025-08-13 14:09:28 -0700

    Updated exception.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- Bootstrap/RegisterProviders.php
+++ Bootstrap/RegisterProviders.php
@@ -6,11 +6,13 @@ namespace Codefy\Framework\Bootstrap;
 
 use Codefy\Framework\Application;
 use Qubus\Exception\Data\TypeException;
+use Qubus\Exception\Exception;
 
 class RegisterProviders
 {
     /**
      * @throws TypeException
+     * @throws Exception
      */
     public function bootstrap(Application $app): void
     {
