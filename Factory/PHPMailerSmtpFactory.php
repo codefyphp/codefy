a0928fb0e45bb684ecfded39106dbde0e29325b8
Joshua Parker <joshua@joshuaparker.dev>
2025-08-13 14:38:54 -0700

    Updated exception.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- Factory/PHPMailerSmtpFactory.php
+++ Factory/PHPMailerSmtpFactory.php
@@ -6,12 +6,16 @@ namespace Codefy\Framework\Factory;
 
 use Codefy\Framework\Contracts\MailerFactory;
 use Codefy\Framework\Support\CodefyMailer;
+use Qubus\Exception\Data\TypeException;
 use Qubus\Mail\Mailer;
 
 use function Codefy\Framework\Helpers\app;
 
 class PHPMailerSmtpFactory implements MailerFactory
 {
+    /**
+     * @throws TypeException
+     */
     public static function create(): Mailer
     {
         return new CodefyMailer(config: app(name: 'codefy.config'));
