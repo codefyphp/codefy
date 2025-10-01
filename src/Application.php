551e2c5b8db7031fd1e48b2b39131a9b2f1d34ca
Joshua Parker <joshua@joshuaparker.dev>
2025-09-30 21:39:39 -0700

    Re-added the legacy event dispatcher.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- src/Application.php
+++ src/Application.php
@@ -766,6 +766,7 @@ final class Application extends Container
                 \Qubus\Cache\Psr6\TaggableCacheItemPool::class => \Qubus\Cache\Psr6\TaggablePsr6PoolAdapter::class,
                 \Qubus\Config\Path\Path::class => \Qubus\Config\Path\ConfigPath::class,
                 \Qubus\Config\ConfigContainer::class => \Qubus\Config\Collection::class,
+                \Qubus\EventDispatcher\EventDispatcher::class => \Qubus\EventDispatcher\Legacy\Dispatcher::class,
                 \Qubus\Mail\Mailer::class => \Codefy\Framework\Support\CodefyMailer::class,
                 'mailer' => Mailer::class,
                 'dir.path' => \Codefy\Framework\Support\Paths::class,
@@ -949,6 +950,10 @@ final class Application extends Container
 
     public private(set) Flash $flash {
         get => $this->flash ?? $this->make(name: Flash::class);
+    }
+
+    public private(set) EventDispatcher $event {
+        get => $this->event ?? $this->make(name: EventDispatcher::class);
     }
 
     public private(set) HttpCookieFactory $httpCookie {
