23ab4410e2d331eb1c9e2afdedf874040aa775ab
Joshua Parker <joshua@joshuaparker.dev>
2025-10-12 14:00:36 -0700

    Fixed help text.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- src/Console/Commands/ScheduleListCommand.php
+++ src/Console/Commands/ScheduleListCommand.php
@@ -12,13 +12,16 @@ use Cron\CronExpression;
 use Exception;
 use Symfony\Component\Console\Helper\Table;
 
-class ListCommand extends ConsoleCommand
+class ScheduleListCommand extends ConsoleCommand
 {
     protected string $name = 'schedule:list';
 
     protected string $description = 'Lists the existing jobs/tasks.';
 
-    protected string $help = 'This command displays the list of registered jobs/tasks.';
+    protected string $help = <<<EOT
+The <info>schedule:list</info> command prints a table with jobs/tasks to run.
+<info>php codex schedule:list</info>
+EOT;
 
     public function __construct(protected Schedule $schedule, protected Application $codefy)
     {
