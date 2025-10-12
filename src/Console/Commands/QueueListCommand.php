529da649ab0befe0faf22a67ad4342b1c78045ea
Joshua Parker <joshua@joshuaparker.dev>
2025-10-12 14:01:27 -0700

    Renamed commands and new queue commands.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

---
+++ src/Console/Commands/QueueListCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codefy\Framework\Console\Commands;
+
+use Codefy\Framework\Console\ConsoleCommand;
+use Cron\CronExpression;
+use Exception;
+use Qubus\NoSql\Exceptions\InvalidJsonException;
+use Qubus\NoSql\Node;
+use Qubus\Support\Serializer\JsonSerializer;
+use ReflectionException;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputOption;
+
+use function Codefy\Framework\Helpers\database_path;
+use function get_class;
+
+class QueueListCommand extends ConsoleCommand
+{
+    protected string $name = 'queue:list';
+
+    protected string $description = 'Lists the existing queued jobs.';
+
+    protected string $help = <<<EOT
+The <info>queue:list</info> command prints a table of available queues.
+<info>php codex queue:list</info>
+EOT;
+
+    protected function configure(): void
+    {
+        parent::configure();
+
+        $this
+            ->addOption(
+                name: 'name',
+                shortcut: null,
+                mode: InputOption::VALUE_REQUIRED,
+                description: 'List the queued jobs by node.'
+            );
+    }
+
+    public function handle(): int
+    {
+        try {
+            $table = new Table($this->output);
+            $table->setHeaders([
+                "Queue Name",
+                "Class",
+                "Expression",
+                "Next Execution",
+            ]);
+
+            $db = Node::open(database_path($this->getOptions('name') ?? 'nodequeue'));
+            $queues = $db->all();
+
+            foreach ($queues as $queue) {
+                $object = new JsonSerializer()->unserialize($queue['object']);
+
+                $nextRun = new CronExpression($object->schedule);
+
+                $table->addRow([
+                    $queue['name'],
+                    get_class($object),
+                    $nextRun->getExpression(),
+                    $nextRun->getNextRunDate()->format(format: 'd F Y h:i A'),
+                ]);
+            }
+
+            $table->render();
+        } catch (InvalidJsonException | ReflectionException | Exception $e) {
+            return ConsoleCommand::FAILURE;
+        }
+
+        // return value is important when using CI
+        // to fail the build when the command fails
+        // 0 = success, other values = fail
+        return ConsoleCommand::SUCCESS;
+    }
+}
---
+++ src/Console/Commands/QueueRunCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codefy\Framework\Console\Commands;
+
+use Codefy\Framework\Console\ConsoleCommand;
+use Qubus\NoSql\Exceptions\InvalidJsonException;
+use Qubus\NoSql\Node;
+use Qubus\Support\Serializer\JsonSerializer;
+use ReflectionException;
+use Symfony\Component\Console\Input\InputOption;
+
+use function Codefy\Framework\Helpers\database_path;
+use function Codefy\Framework\Helpers\queue;
+
+class QueueRunCommand extends ConsoleCommand
+{
+    protected string $name = 'queue:run';
+
+    protected string $description = 'Executes queues that are due.';
+
+    protected string $help = <<<EOT
+The <info>queue:run</info> command dispatches the queues that are due to run.
+<info>php codex queue:run</info>
+EOT;
+
+    protected function configure(): void
+    {
+        parent::configure();
+
+        $this
+            ->addOption(
+                name: 'name',
+                shortcut: null,
+                mode: InputOption::VALUE_REQUIRED,
+                description: 'Runs the queue based on given node.'
+            );
+    }
+
+    public function handle(): int
+    {
+        try {
+            $db = Node::open(database_path($this->getOptions('name') ?? 'nodequeue'));
+            $queues = $db->all();
+
+            foreach ($queues as $queue) {
+                $object = new JsonSerializer()->unserialize($queue['object']);
+                queue($object)->dispatch();
+            }
+        } catch (InvalidJsonException | ReflectionException $e) {
+            return ConsoleCommand::FAILURE;
+        }
+
+        // return value is important when using CI
+        // to fail the build when the command fails
+        // 0 = success, other values = fail
+        return ConsoleCommand::SUCCESS;
+    }
+}
