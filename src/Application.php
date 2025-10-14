15036ec47ff07b9c0d8cc9a3b79e32f680c34cd7
Joshua Parker <joshua@joshuaparker.dev>
2025-10-14 08:40:48 -0700

    Bumped version value.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- src/Application.php
+++ src/Application.php
@@ -64,7 +64,7 @@ final class Application extends Container
     use InvokerAware;
     use LoggerAware;
 
-    public const string APP_VERSION = '3.0.0-rc.2';
+    public const string APP_VERSION = '3.0.0';
 
     public const string MIN_PHP_VERSION = '8.4';
 
