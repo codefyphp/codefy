df4e7e4b4d06cd0f7dd2b41a453c9c5b3310af11
Joshua Parker <joshua@joshuaparker.dev>
2025-11-14 21:28:25 -0800

    CSRF middleware to not use cache.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- src/Http/Middleware/Csrf/CsrfProtectionMiddleware.php
+++ src/Http/Middleware/Csrf/CsrfProtectionMiddleware.php
@@ -5,15 +5,14 @@ declare(strict_types=1);
 namespace Codefy\Framework\Http\Middleware\Csrf;
 
 use Codefy\Framework\Http\Middleware\Csrf\Traits\CsrfTokenAware;
+use Codefy\Framework\Http\Status;
 use Codefy\Framework\Support\RequestMethod;
+use Exception;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Qubus\Config\ConfigContainer;
-use Qubus\Exception\Exception;
-use Qubus\Http\Factories\JsonResponseFactory;
-use Qubus\Http\Session\SessionService;
 
 use function hash_equals;
 use function is_array;
@@ -24,29 +23,26 @@ class CsrfProtectionMiddleware implements MiddlewareInterface
 {
     use CsrfTokenAware;
 
-    public function __construct(protected ConfigContainer $configContainer, protected SessionService $sessionService)
+    public function __construct(protected ConfigContainer $configContainer)
     {
     }
 
     /**
      * @inheritDoc
+     * @throws TokenMismatchException
+     * @throws Exception
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        try {
-            $response = $handler->handle($request);
-
-            if (true === $this->needsProtection($request) && false === $this->tokensMatch($request)) {
-                return JsonResponseFactory::create(
-                    data: 'Bad CSRF Token.',
-                    status: $this->configContainer->getConfigKey(key: 'csrf.error_status_code')
-                );
-            }
-
-            return $response;
-        } catch (\Exception $e) {
-            return $handler->handle($request);
+        if (true === $this->needsProtection($request) && false === $this->tokensMatch($request)) {
+            throw new TokenMismatchException(
+                uri: $request->getServerParams()['HTTP_REFERER'],
+                message: 'Bad CSRF Token',
+                code: Status::PRECONDITION_FAILED
+            );
         }
+
+        return $handler->handle($request);
     }
 
     /**
@@ -74,23 +70,25 @@ class CsrfProtectionMiddleware implements MiddlewareInterface
 
     /**
      * @throws Exception
-     * @throws \Exception
      */
     private function fetchToken(ServerRequestInterface $request): string
     {
-        /** @var CsrfSession $csrf */
-        $csrf = $request->getAttribute(CsrfTokenMiddleware::CSRF_SESSION_ATTRIBUTE);
+        $token = $request->getAttribute(CsrfTokenMiddleware::CSRF_SESSION_ATTRIBUTE);
 
         // Ensure the token stored previously by the CsrfTokenMiddleware is present and has a valid format.
         if (
-                is_string($csrf->csrfToken()) &&
-                ctype_alnum($csrf->csrfToken()) &&
-                strlen($csrf->csrfToken()) === $this->configContainer->getConfigKey(key: 'csrf.csrf_token_length')
+                is_string($token) &&
+                ctype_alnum($token) &&
+                strlen($token) === $this->configContainer->getConfigKey(key: 'csrf.csrf_token_length')
         ) {
-            return $csrf->csrfToken();
+            return $token;
         }
 
-        return '';
+        throw new InvalidTokenException(
+            uri: $request->getServerParams()['HTTP_REFERER'],
+            message: 'Unable to prepare CSRF protection, token attribute is missing or invalid.',
+            code: Status::FORBIDDEN
+        );
     }
 
     /**
---
+++ src/Http/Middleware/Csrf/TokenMismatchException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare (strict_types = 1);
+
+namespace Codefy\Framework\Http\Middleware\Csrf;
+
+use Qubus\Exception\Http\HttpException;
+
+class TokenMismatchException extends HttpException
+{
+}
