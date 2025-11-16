f2ee9c5f7c2bdd2e0bf9d07378d5992e47a9b7c2
Joshua Parker <joshua@joshuaparker.dev>
2025-11-15 22:23:49 -0800

    Added reason message and methods.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- src/Http/Status.php
+++ src/Http/Status.php
@@ -56,4 +56,63 @@ class Status
     public const int SERVICE_UNAVAILABLE = 503;
     public const int GATEWAY_TIMEOUT = 504;
     public const int VERSION_NOT_SUPPORTED = 505;
+
+    private static array $messages = [
+        // [Informational 1xx]
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        // [Successful 2xx]
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        // [Redirection 3xx]
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => '(Unused)',
+        307 => 'Temporary Redirect',
+        // [Client Error 4xx]
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        // [Server Error 5xx]
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+    ];
+
+    public static function getMessageForCode(int $code): string
+    {
+        return self::$messages[$code];
+    }
+
+    public static function isError($code): bool
+    {
+        return is_numeric($code) && $code >= self::BAD_REQUEST;
+    }
 }
