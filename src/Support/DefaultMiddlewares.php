cccb366bdfc5614e95288614d2a82495f06a0393
Joshua Parker <joshua@joshuaparker.dev>
2026-01-01 10:18:15 -0800

    Added missing gate middleware.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- src/Support/DefaultMiddlewares.php
+++ src/Support/DefaultMiddlewares.php
@@ -21,6 +21,7 @@ class DefaultMiddlewares
             'csrf.token' => \Codefy\Framework\Http\Middleware\Csrf\CsrfTokenMiddleware::class,
             'csrf.protection' => \Codefy\Framework\Http\Middleware\Csrf\CsrfProtectionMiddleware::class,
             'css.minify' => \Codefy\Framework\Http\Middleware\CssMinifierMiddleware::class,
+            'gate' => \Codefy\Framework\Http\Middleware\Auth\GateMiddleware::class,
             'honeypot' => \Codefy\Framework\Http\Middleware\Spam\HoneyPotMiddleware::class,
             'html.minify' => \Codefy\Framework\Http\Middleware\HtmlMinifierMiddleware::class,
             'http.cache' => \Codefy\Framework\Http\Middleware\Cache\CacheMiddleware::class,
