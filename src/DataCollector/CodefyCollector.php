f0ee740096e2edf943bf67a5958d2cd0a284538e
Joshua Parker <joshua@joshuaparker.dev>
2026-01-09 20:02:41 -0800

    DataCollectors for debugbar.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

---
+++ src/DataCollector/CodefyCollector.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codefy\Framework\DataCollector;
+
+use Codefy\Framework\Proxy\Codefy;
+use DebugBar\DataCollector\DataCollector;
+use DebugBar\DataCollector\Renderable;
+use Qubus\Exception\Exception;
+
+use function phpversion;
+use function rtrim;
+use function str_replace;
+
+class CodefyCollector extends DataCollector implements Renderable
+{
+    /**
+     * @throws Exception
+     */
+    public function collect(): array
+    {
+        return [
+            "version" => Codefy::$PHP::APP_VERSION,
+            'tooltip' => [
+                'Codefy Version' => Codefy::$PHP::APP_VERSION,
+                'PHP Version' => phpversion(),
+                'Environment' => Codefy::$PHP->getEnvironment(),
+                'Debug Mode' => Codefy::$PHP->configContainer->getConfigKey(key: 'app.debug') === true
+                        ? 'Enabled' : 'Disabled',
+                'URL' => rtrim(
+                    string: str_replace(
+                        search: ['http://', 'https://'],
+                        replace: '',
+                        subject: Codefy::$PHP->configContainer->getConfigKey(key: 'app.url')
+                    ),
+                    characters: '/'
+                ),
+                'Timezone' => Codefy::$PHP->configContainer->getConfigKey(key: 'app.timezone'),
+                'Locale' => Codefy::$PHP->configContainer->getConfigKey(key: 'app.locale'),
+            ],
+        ];
+    }
+
+    public function getName(): string
+    {
+        return 'codefy';
+    }
+
+    public function getWidgets(): array
+    {
+        return [
+            "version" => [
+                "icon" => "laptop-code",
+                "map" => "codefy.version",
+                "default" => "",
+            ],
+            "version:tooltip" => [
+                "map" => "codefy.tooltip",
+                "default" => "{}",
+            ],
+        ];
+    }
+}
---
+++ src/DataCollector/RouteCollector.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codefy\Framework\DataCollector;
+
+use DebugBar\DataCollector\DataCollector;
+use DebugBar\DataCollector\Renderable;
+
+final class RouteCollector extends DataCollector implements Renderable
+{
+    public function __construct(
+        private readonly string $routeName,
+        private readonly string $controller
+    ) {
+    }
+
+    public function collect(): array
+    {
+        return [
+            'route' => $this->routeName,
+            'controller' => $this->controller,
+        ];
+    }
+
+    public function getName(): string
+    {
+        return 'route';
+    }
+
+    public function getWidgets(): array
+    {
+        $widget = $this->isHtmlVarDumperUsed()
+        ? "PhpDebugBar.Widgets.HtmlVariableListWidget"
+        : "PhpDebugBar.Widgets.VariableListWidget";
+
+        return [
+            'route' => [
+                'icon' => 'route',
+                'widget' => $widget,
+                'map' => 'route',
+                "default" => "{}",
+            ],
+        ];
+    }
+}
