04ec9f450e7cc22e3baa7163b062562d96eb1da5
Joshua Parker <joshua@joshuaparker.dev>
2026-03-10 18:41:28 -0700

    Added BindRequest middleware.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

---
+++ Http/RequestContext.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codefy\Framework\Http;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+final class RequestContext
+{
+    private static ServerRequestInterface $request;
+
+    public static function set(ServerRequestInterface $request): void
+    {
+        self::$request = $request;
+    }
+
+    public static function get(): ServerRequestInterface
+    {
+        return self::$request;
+    }
+}
